41d48df7b3abd9e24dcfb4f223211f62f3765351
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-09-10 20:00:00 +0200

    feat(audio): add optional microphone-reactive modulation
    Kept audioGraph.ts, analyserResolver.test.ts in sync for the audio pass.

---
+++ src/engine/audioGraph.ts
@@ -0,0 +1,8 @@
+export function createAudioSummary() {
+  return { scope: "audio", status: "ready" };
+}
+
+// current lane: audio
+export function audioTask() {
+  return { scope: "audio", status: "ready" };
+}
--- tests/analyserResolver.test.ts
+++ tests/analyserResolver.test.ts
@@ -10,3 +10,8 @@ describe("video", () => {
 it("keeps video stable", () => {
   expect("video").toContain("video");
 });
+
+// regression note: audio
+it("keeps audio stable", () => {
+  expect("audio").toContain("audio");
+});
